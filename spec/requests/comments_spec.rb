3f9b845025ba1ef340fd099bf6f03385c8eb8fd1
Enigel <enigel@gmail.com>
2013-11-04 02:07:16 +0000

    shouldn't

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -129,13 +129,11 @@ describe "Comments" do
     it "should not be able to be replied to by guests on a work" do
       visit "/works/#{@work.id}/comments/#{@comment.id}"
       should_not have_button "Reply"
-      # should have_content("Sorry, this work doesn't allow non-Archive users to comment. You can however still leave Kudos!")
     end
 
     it "should not be able to be replied to by guests on a work's chapter" do
       visit "/works/#{@work.id}/chapters/#{@work.chapters.last.id}/comments/#{@comment.id}"
       should_not have_button "Reply"
-      # should have_content("Sorry, this work doesn't allow non-Archive users to comment. You can however still leave Kudos!")
     end
   end
 end
