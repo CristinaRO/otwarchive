acea4682eedb83df735bdc102e2dbfe09646300e
Matt Sears <matt@mattsears.com>
2019-02-19 10:07:19 -0800

    Removes unneeded class name checks for Archive Warning classes.

--- lib/taggable.rb
+++ lib/taggable.rb
@@ -265,7 +265,7 @@ module Taggable
 
   # Simple name to make it easier for people to use in full-text search
   def tag
-    (tags + filters).uniq.map{ |t| t.name == "Warning" ? "ArchiveWarning" : t.name}
+    (tags + filters).uniq.map{ |t| t.name }
   end
 
   # Index all the filters for pulling works
