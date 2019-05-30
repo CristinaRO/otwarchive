72d63c6bd2795ac634f258e0e6dec3c7b6c66069
Matt Sears <matt@mattsears.com>
2019-05-30 10:58:09 -0700

    WIP: Adds prototype for tag typle labeling.

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -155,6 +155,14 @@ module TagsHelper
     ArchiveWarning::DISPLAY_NAME_MAPPING[name] || name
   end
 
+  def tag_type_label_name(tag_type)
+    if tag_type == 'archive_warning'
+      "Warnings"
+    else
+      tag_type.humanize.titleize.pluralize
+    end
+  end
+
   # Individual results for a tag search
   def tag_search_result(tag)
     if tag
