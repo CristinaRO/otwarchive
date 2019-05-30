51da34225ec4eda5e52d184d8d25062dd0f895d3
Matt Sears <matt@mattsears.com>
2019-05-30 14:18:01 -0700

    Updates code to unify a method that renders the appropriate value for the tag type label.

--- app/models/archive_warning.rb
+++ app/models/archive_warning.rb
@@ -19,6 +19,10 @@ class ArchiveWarning < Tag
     warning_tags.include? warning
   end
 
+  def self.label_name
+    "Warnings"
+  end
+
   def display_name
     DISPLAY_NAME_MAPPING[name] || name
   end
